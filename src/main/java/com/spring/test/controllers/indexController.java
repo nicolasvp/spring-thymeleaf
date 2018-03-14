6e0b82bdce16deb9049326c30e58573a564bbde3
nicolasvp <nicolasverapalominos@gmail.com>
2018-03-14 23:49:50 +0100

    agregada la vista principal

---
+++ src/main/java/com/spring/test/controllers/indexController.java
@@ -0,0 +1,16 @@
+package com.spring.test.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+
+	@GetMapping("/")
+	public String hello(Model model) {
+		model.addAttribute("message","Hola pal que lee!");
+		
+		return "index";
+	}
+}
