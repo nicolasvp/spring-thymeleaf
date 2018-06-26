0da570583a051d62c232283172186ce7d2e87f0a
nicolasvp <nicolasverapalominos@gmail.com>
2018-06-26 23:34:36 +0200

    customizacion de vista login

--- src/main/java/com/spring/test/SpringSecurityConfig.java
+++ src/main/java/com/spring/test/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.spring.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,26 +13,34 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
-	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception
-	{
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
-		
-		build.inMemoryAuthentication()
-		.withUser(users.username("admin").password("123123").roles("admin","user"))
-		.withUser(users.username("nicolas").password("123123").roles("user"));
-		
-	}
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
 		http.authorizeRequests().antMatchers("/","/css/**","/js/**","/images/**","/plugins/**","/less/**","/bootstrap/**").permitAll()
 		.antMatchers("/show/**").hasAnyRole("user")
 		.antMatchers("/uploads/**").hasAnyRole("user")
 		.antMatchers("/form/**").hasAnyRole("admin")
 		.antMatchers("/delete/**").hasAnyRole("admin")
 		.antMatchers("/bill/**").hasAnyRole("admin")
-		.anyRequest().authenticated();
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().loginPage("/login")
+		.permitAll()
+		.and()
+		.logout().permitAll();
+		
+	}
+	
+	@Autowired
+	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception
+	{
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+		
+		build.inMemoryAuthentication()
+		.withUser(users.username("admin").password("12345").roles("admin","user"))
+		.withUser(users.username("nicolas").password("12345").roles("user"));
+		
 	}
 	
 }
---
+++ src/main/java/com/spring/test/controllers/loginController.java
@@ -0,0 +1,29 @@
+package com.spring.test.controllers;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class loginController {
+	
+	@GetMapping("/login")
+	public String login(@RequestParam(value="error", required=false) String error, Model model, Principal principal, RedirectAttributes flash) {
+		
+		// Evita el doble inicio de sesión redirigiendo al index
+		if(principal != null) {
+			flash.addFlashAttribute("info", "Su sesión aún se encuentra activa");
+			return "redirect:/";
+		}
+		
+		if(error != null) {
+			model.addAttribute("error", "Las credenciales ingresadas no coinciden con nuestros registros");
+		}
+		
+		return "login";
+	}
+}
